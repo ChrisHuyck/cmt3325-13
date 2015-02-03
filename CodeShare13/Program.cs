64f5743575b46b96c0cb8c88f7613de5c9ff208a
Dainius <dk471@live.mdx.ac.uk>
2015-02-03 13:51:37 +0000

    Commiting again

--- CodeShare13/Program.cs
+++ CodeShare13/Program.cs
@@ -18,7 +18,7 @@ namespace CodeShare13
             Console.WriteLine("Chris In lab.");
             Console.WriteLine("Hey Chris");
             Console.WriteLine("Hi Matthew");
-            Console.WriteLine("Hi all, its Dainius");
+            Console.WriteLine("Hi all, its Dainius, why no one see me ?");
             Console.ReadKey();
             
 
